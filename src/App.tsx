b38289e59f7feed49ac99363a880d63ddbd13051
chirsanovalexandr05-gif <kirsalex08@gmail.com>
2026-03-02 00:03:28 +0200

    Added information inside subscriptions

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { MainLayout } from './layout/MainLayout';
 // Pages
 import { Landing } from './pages/Landing';
 import { Courses } from './pages/Courses';
+import { SubscriptionDetails } from './pages/SubscriptionDetails';
 import { Coaches } from './pages/Coaches';
 import { Students } from './pages/Students';
 import { StudentDashboard } from './pages/StudentDashboard';
@@ -91,6 +92,7 @@ function App() {
                   } />
 
                   <Route path="courses" element={<Courses />} />
+                  <Route path="courses/:planId" element={<SubscriptionDetails />} />
                   <Route path="coaches" element={<Coaches />} />
                   <Route path="cart" element={<CartPage />} />
                   <Route path="checkout" element={<Checkout />} />
